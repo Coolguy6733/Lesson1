e9bcdb4093bdd43906b107e7055947317a1c0c70
Coolguy6733 <er.kanikagupta2007@gmail.com>
2026-06-22 10:33:45 -0700

    Add files via upload

---
+++ Activity3_21.py
@@ -0,0 +1,15 @@
+l =[4, 5, 1, 2, 9, 7, 10, 8]
+
+count = 0
+for i in l:
+    count +=i
+
+avg = count/len(l)
+
+print("sum = ", count)
+print("average =", avg)
+
+l.sort()
+
+print("smallest element is:", l[0])
+print("Largest element is", l[-1])
