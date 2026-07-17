603facc1fe040e5cd10598496f85220b62c07f60
Coolguy6733 <er.kanikagupta2007@gmail.com>
2026-07-17 10:03:44 -0700

    Add files via upload

---
+++ Activity2_27.py
@@ -0,0 +1,17 @@
+class Employee:
+    
+    def __init__(self):
+        print("Employee created")
+    
+    def __del__(self):
+        print("Destructor Called")
+    
+def Create_obj():
+    print("Making Object...")
+    obj = Employee()
+    print("function end...")
+    return obj
+
+print("Calling Create Obj() function...")
+obj = Create_obj()
+print("Program End...")
